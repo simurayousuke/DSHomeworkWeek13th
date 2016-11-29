5dc50baf5496eec7287c19b1f2a84a2d7f9778db
庄庄 <yzz0427@vip.qq.com>
2016-11-29 10:09:25 +0800

    Initial Commit

---
+++ DSHomeworkWeek13th/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DSHomeworkWeek13th
+//
+//  Created by 庄庄 on 2016/11/29.
+//  Copyright © 2016年 YangZhiZhuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
